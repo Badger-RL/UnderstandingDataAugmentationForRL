5011cf20ddebefb1a828cbf1f059b1b5cd0d913d
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2022-07-26 12:03:11 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # set up conda env
-mkdir augment_env
-tar -xzf augment.tar.gz -C augment_env
-rm augment.tar.gz # remove env tarball
-source augment_env/bin/activate
+ENVNAME=rl-augment
+ENVDIR=${ENVNAME}_env
+mkdir $ENVDIR
+tar -xzf ${ENVNAME}.tar.gz -C $ENVDIR
+rm ${ENVNAME}.tar.gz # remove env tarball
+source $ENVDIR/bin/activate
 
 # set up  mujoco
 ./mujoco_get_dependencies.sh
 source ./mujoco_setup.sh
 
-git clone -b latent https://github.com/DLR-RM/stable-baselines3.git
+git clone https://github.com/DLR-RM/stable-baselines3.git
 pip install -e stable-baselines3
 
 pip install -e .
