4cc5319ff4c6c6400d4fb594ff3f32b006f241de
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2022-07-29 22:25:23 -0500

    Refactor

--- augment/rl/train.py
+++ augment/rl/train.py
@@ -2,66 +2,59 @@ import argparse
 import difflib
 import os.path
 import uuid
+from operator import itemgetter
 
 import gym
 import numpy as np
 import torch
 import yaml
 from stable_baselines3.common.callbacks import EvalCallback
-from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.monitor import Monitor
 
 from augment.rl.augmentation_functions import HorizontalTranslation
-from augment.rl.utils import get_latest_run_id, ALGOS, read_hyperparameters, StoreDict, preprocess_action_noise
+from augment.rl.utils import ALGOS, StoreDict,get_save_dir, preprocess_action_noise, read_hyperparameters
 from stable_baselines3.common.utils import set_random_seed
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--algo", help="RL Algorithm", default="ddpg", type=str, required=False, choices=list(ALGOS.keys()))
-    parser.add_argument("--env", type=str, default="InvertedPendulum-v2", help="environment ID")
-    parser.add_argument("--run-id", help="Run id to append to env save directory", default=None, type=int)
-    parser.add_argument("-i", "--trained-agent", help="Path to a pretrained agent to continue training", default="", type=str)
-    parser.add_argument("-n", "--n-timesteps", help="Overwrite the number of timesteps", default=int(1e5), type=int)
-    parser.add_argument(
-        "--eval-freq",
-        help="Evaluate the agent every n steps (if negative, no evaluation). "
-        "During hyperparameter optimization n-evaluations is used instead",
-        default=10000,
-        type=int,
-    )
-    parser.add_argument("--eval-episodes", help="Number of episodes to use for evaluation", default=10, type=int)
-    parser.add_argument("--save-freq", help="Save the model every n steps (if negative, no checkpoint)", default=-1, type=int)
-    parser.add_argument("-f", "--log-folder", help="Log folder", type=str, default="results")
-    parser.add_argument("--seed", help="Random generator seed", type=int, default=-1)
-    parser.add_argument("--random-hyperparameters", default=False, help="Sample random hyperparameters for a single run.")
-    parser.add_argument("--verbose", help="Verbose mode (0: no output, 1: INFO)", default=0, type=int)
-    parser.add_argument("--env-kwargs", type=str, nargs="*", action=StoreDict, default={}, help="Optional keyword argument to pass to the env constructor")
-    parser.add_argument(
-        "-params",
-        "--hyperparams",
-        type=str,
-        nargs="+",
-        action=StoreDict,
-        help="Overwrite hyperparameter (e.g. learning_rate:0.01 train_freq:10)",
-    )
-    parser.add_argument(
-        "--add-policy-kwargs", type=str, nargs="*", action=StoreDict, default={}, help="Optional ADDITIONAL keyword argument to pass to the policy constructor"
-    )
-    parser.add_argument("-uuid", "--uuid", action="store_true", default=False, help="Ensure that the run has a unique ID")
 
+    # basic
+    parser.add_argument("--algo", help="RL Algorithm", default="ppo", type=str, required=False, choices=list(ALGOS.keys()))
+    parser.add_argument("--env", type=str, default="InvertedPendulum-v2", help="environment ID")
+    parser.add_argument("--seed", help="Random generator seed", type=int, default=-1)
+    parser.add_argument("-n", "--n-timesteps", help="Overwrite the number of timesteps", default=int(1e5), type=int)
+    parser.add_argument("--eval-freq", help="Evaluate the agent every n steps (if negative, no evaluation).", default=10000, type=int,)
+    parser.add_argument("--eval-episodes", help="Number of episodes to use for evaluation", default=10, type=int)
+    parser.add_argument("-i", "--trained-agent", help="Path to a pretrained agent to continue training", default="", type=str)
+
+    # parameters
+    parser.add_argument("--env-kwargs", type=str, nargs="*", action=StoreDict, default={}, help="Optional keyword argument to pass to the env constructor")
+    parser.add_argument("-params", "--hyperparams", type=str, nargs="+", action=StoreDict, help="Overwrite hyperparameter (e.g. learning_rate:0.01 train_freq:10)" )
     parser.add_argument("-aug-kwargs", "--augmentation-kwargs", type=str, nargs="*", action=StoreDict, default={})
+    parser.add_argument("--add-policy-kwargs", type=str, nargs="*", action=StoreDict, default={}, help="Optional ADDITIONAL keyword argument to pass to the policy constructor" )
+
+    # saving
+    parser.add_argument("-f", "--log-folder", help="Log folder", type=str, default="results")
+    parser.add_argument("--run-id", help="Run id to append to env save directory", default=None, type=int)
+
+    # extra
+    parser.add_argument("--verbose", help="Verbose mode (0: no output, 1: INFO)", default=0, type=int)
+    parser.add_argument("--random-hyperparameters", default=False, help="Sample random hyperparameters for a single run.")
 
     args = parser.parse_args()
 
-    ###################################################################################################################
+    ####################################################################################################################
 
     env_id = args.env
     algo = args.algo
     n_timesteps = args.n_timesteps
 
-    registered_envs = set(gym.envs.registry.env_specs.keys())  # pytype: disable=module-attr
+    ####################################################################################################################
+    # Assertions
+
     # If the environment is not found, suggest the closest match
+    registered_envs = set(gym.envs.registry.env_specs.keys())  # pytype: disable=module-attr
     if env_id not in registered_envs:
         try:
             closest_match = difflib.get_close_matches(env_id, registered_envs, n=1)[0]
@@ -69,71 +62,70 @@ if __name__ == '__main__':
             closest_match = "'no close match found...'"
         raise ValueError(f"{env_id} not found in gym registry, you maybe meant {closest_match}?")
 
-    # Unique id to ensure there is no race condition for the folder creation
-    uuid_str = f"_{uuid.uuid4()}" if args.uuid else ""
-    if args.seed < 0:
-        # Seed but with a random one
-        args.seed = np.random.randint(2 ** 32 - 1, dtype="int64").item()
-
     if args.trained_agent != "":
-        assert args.trained_agent.endswith(".zip") and os.path.isfile(
-            args.trained_agent
-        ), "The trained_agent must be a valid path to a .zip file"
-
-
-    set_random_seed(args.seed)
-
-    save_dir = f'{args.log_folder}/{algo}/{env_id}'
-    save_dir += f'/run_{get_latest_run_id(save_dir) + 1}'
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-    print(f'Results will be saved to {save_dir}')
+        assert args.trained_agent.endswith(".zip") and os.path.isfile(args.trained_agent), "The trained_agent must be a valid path to a .zip file"
 
     ####################################################################################################################
+    # Preprocess args
 
-    env = Monitor(gym.make(env_id, **args.env_kwargs),)
-    algo_class = ALGOS[algo]
+    save_dir = get_save_dir(args.log_folder, env_id, algo, args.run_id)
 
+    # update hyperparams
     hyperparams = read_hyperparameters(env_id, algo)
-    preprocess_action_noise(hyperparams=hyperparams, env=env)
+    if args.hyperparams is not None:
+        hyperparams.update(args.hyperparams)
 
-    if "train_freq" in hyperparams and isinstance(hyperparams["train_freq"], list):
-        hyperparams["train_freq"] = tuple(hyperparams["train_freq"])
+    # set seed
+    if args.seed < 0:
+        args.seed = np.random.randint(2 ** 32 - 1, dtype="int64").item()
+    set_random_seed(args.seed)
 
-    # # Should we overwrite the number of timesteps?
-    if n_timesteps > 0:
+    # set n_timesteps
+    if args.n_timesteps > 0:
         print(f"Overwriting n_timesteps with n={n_timesteps}")
         del hyperparams["n_timesteps"]
     else:
         n_timesteps = int(hyperparams.pop("n_timesteps"))
-    # n_timesteps = int(hyperparams.pop("n_timesteps"))
 
-    print('Automatically scaling replay buffer to account for augmentation')
-    try:
-        num_aug = args.augmentation_kwargs['n']
-    except:
-        num_aug = 1
+    # set train_freq
+    if "train_freq" in hyperparams and isinstance(hyperparams["train_freq"], list):
+        hyperparams["train_freq"] = tuple(hyperparams["train_freq"])
 
-    hyperparams['buffer_size'] *= (num_aug+1)
+    # augmentation
+    augmentation_function = None
+    if args.augmentation_kwargs:
+        print('Automatically scaling replay buffer to account for augmentation')
+        try:
+            num_aug = args.augmentation_kwargs['n']
+        except:
+            num_aug = 1
 
+        hyperparams['buffer_size'] *= (num_aug+1)
+        augmentation_function = HorizontalTranslation(**args.augmentation_kwargs)
 
+    ####################################################################################################################
+    # More preprocessing that depends on the env object
+
+    env = Monitor(gym.make(env_id, **args.env_kwargs),)
+    preprocess_action_noise(hyperparams=hyperparams, env=env)
+
+    algo_class = ALGOS[algo]
     model = algo_class(env=env, **hyperparams)
-
-    augmentation_function = HorizontalTranslation(**args.augmentation_kwargs)
     model.set_augmentation_function(augmentation_function)
 
+    # save hyperparams and args
     with open(os.path.join(save_dir, "config.yml"), "w") as f:
         yaml.dump(hyperparams, f)
+    with open(os.path.join(save_dir, "args.yml"), "w") as f:
+        yaml.dump(args, f)
 
     # Setting num threads to 1 makes things run faster on cpu
     torch.set_num_threads(1)
-
     env_eval = Monitor(gym.make(env_id, **args.env_kwargs), filename=save_dir)
     eval_callback = EvalCallback(eval_env=env_eval, n_eval_episodes=args.eval_episodes, eval_freq=args.eval_freq, log_path=save_dir, best_model_save_path=save_dir)
     model.learn(total_timesteps=int(n_timesteps), callback=eval_callback)
 
     print(f'Results saved to {save_dir}')
-
 
     # custom_objects = {}
     # algo_class = ALGOS[algo]
--- augment/rl/utils.py
+++ augment/rl/utils.py
@@ -12,6 +12,8 @@ import yaml
 
 from stable_baselines3 import A2C, DDPG, DQN, PPO, SAC, TD3
 from augment.rl.algs.ddpg import DDPG
+from augment.rl.algs.ppo import PPO
+from augment.rl.algs.td3 import TD3
 from stable_baselines3.common.callbacks import CheckpointCallback, EvalCallback, BaseCallback
 from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
 from stable_baselines3.common.vec_env import VecEnv
@@ -26,7 +28,6 @@ ALGOS = {
     "sac": SAC,
     "td3": TD3,
 }
-
 
 class StoreDict(argparse.Action):
     """
@@ -49,6 +50,24 @@ class StoreDict(argparse.Action):
             arg_dict[key] = eval(value)
         setattr(namespace, self.dest, arg_dict)
 
+def create_log_folder(self):
+    os.makedirs(self.params_path, exist_ok=True)
+
+def read_hyperparameters(env_id, algo) -> Dict[str, Any]:
+    # Load hyperparameters from yaml file
+    with open(f"hyperparams/{algo}.yml") as f:
+        hyperparams_dict = yaml.safe_load(f)
+        if env_id in list(hyperparams_dict.keys()):
+            hyperparams = hyperparams_dict[env_id]
+        else:
+            raise ValueError(f"Hyperparameters not found for {algo}-{env_id}")
+
+    for kwargs_key in {"policy_kwargs", "replay_buffer_class", "replay_buffer_kwargs"}:
+        if kwargs_key in hyperparams.keys() and isinstance(hyperparams[kwargs_key], str):
+            hyperparams[kwargs_key] = eval(hyperparams[kwargs_key])
+
+    return hyperparams#, saved_hyperparams
+
 def get_latest_run_id(save_dir: str) -> int:
     max_run_id = 0
     for path in glob.glob(os.path.join(save_dir, 'run_[0-9]*')):
@@ -59,99 +78,18 @@ def get_latest_run_id(save_dir: str) -> int:
     return max_run_id
 
 
-def read_hyperparameters(env_id, algo) -> Dict[str, Any]:
-    # Load hyperparameters from yaml file
-    with open(f"hyperparams/{algo}.yml") as f:
-        hyperparams_dict = yaml.safe_load(f)
-        if env_id in list(hyperparams_dict.keys()):
-            hyperparams = hyperparams_dict[env_id]
-        # elif _is_atari:
-        #     hyperparams = hyperparams_dict["atari"]
-        else:
-            raise ValueError(f"Hyperparameters not found for {algo}-{env_id}")
-
-    # if self.custom_hyperparams is not None:
-    #     # Overwrite hyperparams if needed
-    #     hyperparams.update(self.custom_hyperparams)
-    # # Sort hyperparams that will be saved
-    # saved_hyperparams = OrderedDict([(key, hyperparams[key]) for key in sorted(hyperparams.keys())])
-    #
-    # if self.verbose > 0:
-    #     print("Default hyperparameters for environment (ones being tuned will be overridden):")
-    #     pprint(saved_hyperparams)
-
-    for kwargs_key in {"policy_kwargs", "replay_buffer_class", "replay_buffer_kwargs"}:
-        if kwargs_key in hyperparams.keys() and isinstance(hyperparams[kwargs_key], str):
-            hyperparams[kwargs_key] = eval(hyperparams[kwargs_key])
-
-    return hyperparams#, saved_hyperparams
-
-def _preprocess_hyperparams(
-    self, hyperparams: Dict[str, Any]
-) -> Tuple[Dict[str, Any], Optional[Callable], List[BaseCallback], Optional[Callable]]:
-    self.n_envs = hyperparams.get("n_envs", 1)
-
-    if self.verbose > 0:
-        print(f"Using {self.n_envs} environments")
-
-    # Convert schedule strings to objects
-    hyperparams = self._preprocess_schedules(hyperparams)
-
-    # Pre-process train_freq
-    if "train_freq" in hyperparams and isinstance(hyperparams["train_freq"], list):
-        hyperparams["train_freq"] = tuple(hyperparams["train_freq"])
-
-    # Should we overwrite the number of timesteps?
-    if self.n_timesteps > 0:
-        if self.verbose:
-            print(f"Overwriting n_timesteps with n={self.n_timesteps}")
+def get_save_dir(log_folder, env_id, algo, run_id):
+    # set save directory
+    save_dir = f'{log_folder}/{env_id}/{algo}'
+    if run_id:
+        save_dir += f'/run_{run_id}'
     else:
-        self.n_timesteps = int(hyperparams["n_timesteps"])
+        save_dir += f'/run_{get_latest_run_id(save_dir) + 1}'
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    print(f'Results will be saved to {save_dir}')
 
-    # Derive n_evaluations from number of timesteps if needed
-    if self.n_evaluations is None and self.optimize_hyperparameters:
-        self.n_evaluations = max(1, self.n_timesteps // int(1e5))
-        print(
-            f"Doing {self.n_evaluations} intermediate evaluations for pruning based on the number of timesteps."
-            " (1 evaluation every 100k timesteps)"
-        )
-
-    # Pre-process normalize config
-    hyperparams = self._preprocess_normalization(hyperparams)
-
-    # Pre-process policy/buffer keyword arguments
-    # Convert to python object if needed
-    for kwargs_key in {"policy_kwargs", "replay_buffer_class", "replay_buffer_kwargs"}:
-        if kwargs_key in hyperparams.keys() and isinstance(hyperparams[kwargs_key], str):
-            hyperparams[kwargs_key] = eval(hyperparams[kwargs_key])
-
-    # Delete keys so the dict can be pass to the model constructor
-    if "n_envs" in hyperparams.keys():
-        del hyperparams["n_envs"]
-    del hyperparams["n_timesteps"]
-
-    if "frame_stack" in hyperparams.keys():
-        self.frame_stack = hyperparams["frame_stack"]
-        del hyperparams["frame_stack"]
-
-    # obtain a class object from a wrapper name string in hyperparams
-    # and delete the entry
-    env_wrapper = get_wrapper_class(hyperparams)
-    if "env_wrapper" in hyperparams.keys():
-        del hyperparams["env_wrapper"]
-
-    # Same for VecEnvWrapper
-    vec_env_wrapper = get_wrapper_class(hyperparams, "vec_env_wrapper")
-    if "vec_env_wrapper" in hyperparams.keys():
-        del hyperparams["vec_env_wrapper"]
-
-    callbacks = get_callback_list(hyperparams)
-    if "callback" in hyperparams.keys():
-        self.specified_callbacks = hyperparams["callback"]
-        del hyperparams["callback"]
-
-    return hyperparams, env_wrapper, callbacks, vec_env_wrapper
-
+    return save_dir
 
 def preprocess_action_noise(hyperparams: Dict[str, Any], env: VecEnv) -> Dict[str, Any]:
     # Parse noise string
@@ -182,171 +120,3 @@ def preprocess_action_noise(hyperparams: Dict[str, Any], env: VecEnv) -> Dict[st
         del hyperparams["noise_std"]
 
     return hyperparams
-
-def create_log_folder(self):
-    os.makedirs(self.params_path, exist_ok=True)
-
-def create_callbacks(self):
-
-    if self.save_freq > 0:
-        # Account for the number of parallel environments
-        self.save_freq = max(self.save_freq // self.n_envs, 1)
-        self.callbacks.append(
-            CheckpointCallback(
-                save_freq=self.save_freq,
-                save_path=self.save_path,
-                name_prefix="rl_model",
-                verbose=1,
-            )
-        )
-
-    # Create test env if needed, do not normalize reward
-    if self.eval_freq > 0 and not self.optimize_hyperparameters:
-        # Account for the number of parallel environments
-        self.eval_freq = max(self.eval_freq // self.n_envs, 1)
-
-        if self.verbose > 0:
-            print("Creating test environment")
-
-        save_vec_normalize = SaveVecNormalizeCallback(save_freq=1, save_path=self.params_path)
-        eval_callback = EvalCallback(
-            self.create_envs(self.n_eval_envs, eval_env=True),
-            callback_on_new_best=save_vec_normalize,
-            best_model_save_path=self.save_path,
-            n_eval_episodes=self.n_eval_episodes,
-            log_path=self.save_path,
-            eval_freq=self.eval_freq,
-            deterministic=self.deterministic_eval,
-        )
-
-        self.callbacks.append(eval_callback)
-
-
-
-def get_wrapper_class(hyperparams: Dict[str, Any]) -> Optional[Callable[[gym.Env], gym.Env]]:
-    """
-    Get one or more Gym environment wrapper class specified as a hyper parameter
-    "env_wrapper".
-    e.g.
-    env_wrapper: gym_minigrid.wrappers.FlatObsWrapper
-
-    for multiple, specify a list:
-
-    env_wrapper:
-        - utils.wrappers.PlotActionWrapper
-        - utils.wrappers.TimeFeatureWrapper
-
-
-    :param hyperparams:
-    :return: maybe a callable to wrap the environment
-        with one or multiple gym.Wrapper
-    """
-
-    def get_module_name(wrapper_name):
-        return ".".join(wrapper_name.split(".")[:-1])
-
-    def get_class_name(wrapper_name):
-        return wrapper_name.split(".")[-1]
-
-    if "env_wrapper" in hyperparams.keys():
-        wrapper_name = hyperparams.get("env_wrapper")
-
-        if wrapper_name is None:
-            return None
-
-        if not isinstance(wrapper_name, list):
-            wrapper_names = [wrapper_name]
-        else:
-            wrapper_names = wrapper_name
-
-        wrapper_classes = []
-        wrapper_kwargs = []
-        # Handle multiple wrappers
-        for wrapper_name in wrapper_names:
-            # Handle keyword arguments
-            if isinstance(wrapper_name, dict):
-                assert len(wrapper_name) == 1, (
-                    "You have an error in the formatting "
-                    f"of your YAML file near {wrapper_name}. "
-                    "You should check the indentation."
-                )
-                wrapper_dict = wrapper_name
-                wrapper_name = list(wrapper_dict.keys())[0]
-                kwargs = wrapper_dict[wrapper_name]
-            else:
-                kwargs = {}
-            wrapper_module = importlib.import_module(get_module_name(wrapper_name))
-            wrapper_class = getattr(wrapper_module, get_class_name(wrapper_name))
-            wrapper_classes.append(wrapper_class)
-            wrapper_kwargs.append(kwargs)
-
-        def wrap_env(env: gym.Env) -> gym.Env:
-            """
-            :param env:
-            :return:
-            """
-            for wrapper_class, kwargs in zip(wrapper_classes, wrapper_kwargs):
-                env = wrapper_class(env, **kwargs)
-            return env
-
-        return wrap_env
-    else:
-        return None
-
-
-def get_callback_list(hyperparams: Dict[str, Any]) -> List[BaseCallback]:
-    """
-    Get one or more Callback class specified as a hyper-parameter
-    "callback".
-    e.g.
-    callback: stable_baselines3.common.callbacks.CheckpointCallback
-
-    for multiple, specify a list:
-
-    callback:
-        - utils.callbacks.PlotActionWrapper
-        - stable_baselines3.common.callbacks.CheckpointCallback
-
-    :param hyperparams:
-    :return:
-    """
-
-    def get_module_name(callback_name):
-        return ".".join(callback_name.split(".")[:-1])
-
-    def get_class_name(callback_name):
-        return callback_name.split(".")[-1]
-
-    callbacks = []
-
-    if "callback" in hyperparams.keys():
-        callback_name = hyperparams.get("callback")
-
-        if callback_name is None:
-            return callbacks
-
-        if not isinstance(callback_name, list):
-            callback_names = [callback_name]
-        else:
-            callback_names = callback_name
-
-        # Handle multiple wrappers
-        for callback_name in callback_names:
-            # Handle keyword arguments
-            if isinstance(callback_name, dict):
-                assert len(callback_name) == 1, (
-                    "You have an error in the formatting "
-                    f"of your YAML file near {callback_name}. "
-                    "You should check the indentation."
-                )
-                callback_dict = callback_name
-                callback_name = list(callback_dict.keys())[0]
-                kwargs = callback_dict[callback_name]
-            else:
-                kwargs = {}
-            callback_module = importlib.import_module(get_module_name(callback_name))
-            callback_class = getattr(callback_module, get_class_name(callback_name))
-            callbacks.append(callback_class(**kwargs))
-
-    return callbacks
-
