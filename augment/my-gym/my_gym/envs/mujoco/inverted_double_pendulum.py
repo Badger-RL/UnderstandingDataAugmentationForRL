93ebae2be02a1541205fcc3d1c333c2478a6e329
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2022-08-09 21:59:18 -0500

    Double pendulum

--- augment/my-gym/my_gym/envs/mujoco/inverted_double_pendulum.py
+++ augment/my-gym/my_gym/envs/mujoco/inverted_double_pendulum.py
@@ -110,7 +110,53 @@ class InvertedDoublePendulumEnv(InvertedDoublePendulumEnv_original):
 
     """
 
-    def __init__(self):
+    def __init__(self, init_pos=None, use_pos=True):
+        self.init_pos = init_pos
+        self.use_pos = use_pos
+        print(f'init_pos = {init_pos}')
+        print(f'use_pos = {use_pos}')
         super().__init__()
 
+    def step(self, action):
+        self.do_simulation(action, self.frame_skip)
+        ob = self._get_obs()
+        x, _, y = self.sim.data.site_xpos[0]
+        dist_penalty = 0.01 * x ** 2 + (y - 2) ** 2
+        v1, v2 = self.sim.data.qvel[1:3]
+        vel_penalty = 1e-3 * v1 ** 2 + 5e-3 * v2 ** 2
+        alive_bonus = 10
+        r = alive_bonus - dist_penalty - vel_penalty
+        done = bool(y <= 1)
+        return ob, r, done, {}
+
+    def _get_obs(self):
+        x_pos = self.sim.data.qpos[:1] if self.use_pos else [0]
+        return np.concatenate(
+            [
+                x_pos,  # cart x pos
+                np.sin(self.sim.data.qpos[1:]),  # link angles
+                np.cos(self.sim.data.qpos[1:]),
+                np.clip(self.sim.data.qvel, -10, 10),
+                np.clip(self.sim.data.qfrc_constraint, -10, 10),
+            ]
+        ).ravel()
+
+    def reset_model(self):
+        if self.init_pos:
+            qpos = self.init_qpos + self.np_random.uniform(low=-0.1, high=0.1, size=self.model.nq)
+            qpos[0] = self.init_qpos[0] + self.np_random.uniform(low=self.init_pos[0], high=self.init_pos[1])
+        else:
+            qpos = self.init_qpos + self.np_random.uniform(low=-0.1, high=0.1, size=self.model.nq)
+
+        self.set_state(
+            qpos,
+            self.init_qvel + self.np_random.randn(self.model.nv) * 0.1,
+        )
+        return self._get_obs()
+
+    def viewer_setup(self):
+        v = self.viewer
+        v.cam.trackbodyid = 0
+        v.cam.distance = self.model.stat.extent * 0.5
+        v.cam.lookat[2] = 0.12250000000000005  # v.model.stat.center[2]
 
