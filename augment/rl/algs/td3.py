06f49524eb8db941f2648d5a653695195d0c90b5
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2022-10-24 14:16:11 -0500

    Remove old imports

--- augment/rl/algs/td3.py
+++ augment/rl/algs/td3.py
@@ -6,7 +6,7 @@ import numpy as np
 import torch as th
 from torch.nn import functional as F
 
-# from stable_baselines3.common.buffers import ReplayBuffer
+from stable_baselines3.common.buffers import ReplayBuffer
 from stable_baselines3.common.noise import ActionNoise
 # from stable_baselines3.common.off_policy_algorithm import OffPolicyAlgorithm
 from stable_baselines3.common.policies import BasePolicy
@@ -14,7 +14,7 @@ from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback, Schedul
 from stable_baselines3.common.utils import polyak_update
 from stable_baselines3.td3.policies import CnnPolicy, MlpPolicy, MultiInputPolicy, TD3Policy
 
-from augment.rl.algs.buffers import ReplayBuffer
+# from augment.rl.algs.buffers import ReplayBuffer
 from augment.rl.algs.off_policy_algorithm import OffPolicyAlgorithmAugment
 # from augment.rl.algs.policies import TD3Policy, MlpPolicy
 # from augment.rl.algs.rbf_policy import RBFExtractor
