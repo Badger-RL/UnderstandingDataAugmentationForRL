6eea27c979008595dbf3be716e790e9804e0491a
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2022-11-02 17:57:45 -0500

    Default no aug

--- augment/rl/train.py
+++ augment/rl/train.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
 
 
     # augmentation
-    parser.add_argument("--aug-function", type=str, default='translate')
+    parser.add_argument("--aug-function", type=str, default=None)
     parser.add_argument("--aug-function-kwargs", type=str, nargs="*", action=StoreDict, default={})
     parser.add_argument("--aug-n", type=float, default=1)
     parser.add_argument("--aug-ratio", type=float, default=1)
