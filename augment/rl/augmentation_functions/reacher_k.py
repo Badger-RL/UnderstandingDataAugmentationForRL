1d1f2c9c4769260a4780f7dd6b08debc75618dc9
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2022-11-04 00:40:55 -0500

    Reacher

--- augment/rl/augmentation_functions/reacher_k.py
+++ augment/rl/augmentation_functions/reacher_k.py
@@ -86,7 +86,7 @@ class ReacherReflect(AugmentationFunction):
     Rotate arm and goal.
     '''
     def __init__(self, k=2, **kwargs):
-        super().__init__()
+        super().__init__(**kwargs)
         self.k = k
         self.sparse = self.env.sparse
         print(self.k)
@@ -96,6 +96,11 @@ class ReacherReflect(AugmentationFunction):
         else:
             self._reward_function = self._set_dense_reward
 
+        self.mask = np.zeros(self.k*2+2+3).astype(bool)
+        self.mask[:2*self.k] = True
+        self.mask[2*self.k+2:-3] = True
+
+
     def _set_reward(self, reward, fingertip_dist, action):
         self._reward_function(reward, fingertip_dist, action)
 
@@ -104,17 +109,6 @@ class ReacherReflect(AugmentationFunction):
 
     def _set_dense_reward(self, reward, fingertip_dist, action):
         reward[:] = -np.linalg.norm(fingertip_dist) * self.k - np.square(action).sum()
-
-    def _rotate_goal(self, obs, theta):
-        x = np.copy(obs[:, 2*self.k])
-        y = np.copy(obs[:, 2*self.k+1])
-        obs[:, 2*self.k]   = x * np.cos(theta) - y * np.sin(theta)
-        obs[:, 2*self.k+1] = x * np.sin(theta) + y * np.cos(theta)
-
-    def _rotate_central_joint(self, obs, theta):
-        theta_curr = np.arctan2(obs[:, self.k], obs[:, 0])
-        obs[:, 0] = np.cos(theta_curr + theta)
-        obs[:, self.k] = np.sin(theta_curr + theta)
 
     def _augment(self,
                 obs: np.ndarray,
@@ -127,24 +121,68 @@ class ReacherReflect(AugmentationFunction):
                 p=None
                 ):
 
+        # delta_qpos = next_obs[:, :2*self.k] - obs[:, :2*self.k]
+        # delta_qvel = next_obs[:, 2*self.k+2:-3] - obs[:, 2*self.k+2:-3]
+        # sin(A+B) = sin A cos B + cos A sin B
+        # cos(A+B) = cos A cos B − sin A sin B
+
+        theta = infos[0][0]['theta']
+        theta_next = infos[0][0]['theta_next']
+        delta_theta = theta_next - theta
+
+        next_obs[:, :self.k] = np.cos(theta-delta_theta)
+        next_obs[:, self.k:self.k*2] = np.sin(theta-delta_theta)
+        next_obs[:, 2*self.k+2:-3] *= -1
+        next_obs[:, -3:] -= 2*(next_obs[:, -3:] - obs[:, -3:])
+
         action[:] *= -1
 
-        # compute delta theta
-        theta = np.arctan2(obs[:, self.k], obs[:, 0])
-        theta_next = np.arctan2(next_obs[:, self.k], next_obs[:, 0])
-        delta_theta = theta_next-theta
+        # delta = next_obs - obs
+        # next_obs[:, :2*self.k] -= 2*delta[:, :2*self.k]
+        # next_obs[:, 2*self.k+2:-3] -= 2*delta[:, 2*self.k+2:-3]
 
-        # reverse delta theta
-        next_obs[:, 0] = np.cos(theta - delta_theta)
-        next_obs[:, self.k] = np.sin(theta - delta_theta)
 
-        # compute delta fingertip dist
-        fingertip_dist = obs[:, -3:-1]
-        fingertip_dist_next = next_obs[:, -3:-1]
-        delta_fingertip_dist = fingertip_dist_next - fingertip_dist
-
-        # reverse delta fingertip dist
-        next_obs[:, -3:-1] -= delta_fingertip_dist
         self._set_reward(reward, next_obs[:, -3:1], action)
 
         return obs, next_obs, action, reward, done, infos
+
+if __name__ == "__main__":
+    k = 2
+    env = gym.make(f'Reacher{k}-v3')
+    f = ReacherReflect(env=env, k=k)
+
+    obs = env.reset()
+    for i in range(1000):
+        obs = env.reset()
+        action = np.ones(2)
+        # action[0] = 1
+        action = np.random.uniform(size=(2,))
+
+        # qpos = np.zeros(4)
+        # qpos[0] = (i*0.1) %( 2*np.pi)
+        # qvel = np.zeros(4)
+        # env.set_state(qpos, qvel)
+
+        next_obs, reward, done, info = env.step(action)
+        print('normal', next_obs-obs)
+
+        env.reset()
+
+
+        obs = obs.reshape(1,-1)
+        next_obs = next_obs.reshape(1,-1)
+        action = action.reshape(1,-1)
+        reward = np.array([reward])
+        done = np.array([done])
+        info = [[info]]
+
+        aug_obs, aug_next_obs, aug_action, aug_reward, aug_done, aug_info = f.augment(1, obs, next_obs, action, reward, done, info)
+        qpos, qvel = env.obs_to_q(aug_obs[0])
+        env.set_state(qpos, qvel)
+        env.render()
+
+
+        next_obs, reward, done, info = env.step(aug_action[0])
+        print('aug', next_obs-obs)
+
+        env.render()
