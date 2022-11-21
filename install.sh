9e27bb6482371bd8718b444c7c7f04be9dca6496
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2022-11-20 21:42:00 -0600

    simplify

--- install.sh
+++ install.sh
@@ -8,8 +8,11 @@ tar -xzf ${ENVNAME}.tar.gz -C $ENVDIR
 rm ${ENVNAME}.tar.gz # remove env tarball
 source $ENVDIR/bin/activate
 
-git clone git@github.com:NicholasCorrado/Gymnasium-Robotics.git -b no-dict
 
 pip install -e .
-pip install -e augment/my-gym
-pip install -e augment/Gymnasium-Robotics
+
+cd augment
+git clone git@github.com:NicholasCorrado/Gymnasium-Robotics.git -b no-dict
+pip install -e Gymnasium-Robotics
+pip install -e my-gym
+cd ..
