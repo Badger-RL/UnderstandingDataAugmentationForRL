29380edfe3047b0963a1b2c7c73cbaa39709dc17
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2022-11-25 11:37:49 -0600

    Attempt to fix import error

--- augment/rl/augmentation_functions/__init__.py
+++ augment/rl/augmentation_functions/__init__.py
@@ -10,6 +10,7 @@ from augment.rl.augmentation_functions.predator_prey import *
 from augment.rl.augmentation_functions.reacher_k import *
 from augment.rl.augmentation_functions.swimmer_k import SwimmerReflect
 from augment.rl.augmentation_functions.walker2d import Walker2dReflect
+from augment.simulate import simulate
 
 predator_prey_box_augmentation_functions = {
         'rotate': Goal2DRotateRestricted,
@@ -112,3 +113,22 @@ for k in range(2,20+1):
     AUGMENTATION_FUNCTIONS[f'Swimmer{k}'] = {
         'reflect': SwimmerReflect
     }
+
+def validate_augmentation(env, aug_func, check_valid):
+    observations, next_observations, actions, rewards, dones, infos = simulate(
+        model=None, env=env, num_episodes=1, seed=np.random.randint(1,1000000), render=False, flatten=True, verbose=0)
+
+    observations = np.expand_dims(observations, axis=1)
+    next_observations = np.expand_dims(next_observations, axis=1)
+    actions = np.expand_dims(actions, axis=1)
+    rewards = np.expand_dims(rewards, axis=1)
+    dones = np.expand_dims(dones, axis=1)
+    infos = np.expand_dims(infos, axis=1)
+
+    aug_n = 1
+
+    for j in range(observations.shape[0]):
+        aug_obs, aug_next_obs, aug_action, aug_reward, aug_done, aug_info = aug_func.augment(
+            aug_n, observations[j], next_observations[j], actions[j], rewards[j], dones[j], infos[j])
+        for k in range(aug_n):
+            check_valid(env, aug_obs[k], aug_next_obs[k], aug_action[k], aug_reward[k], aug_done[k], aug_info[k])
