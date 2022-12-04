9bf837617aa8d599c01d44ff5f22984ed876d698
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2022-12-04 14:43:57 -0600

    attempt bug fix

--- augment/rl/augmentation_functions/panda.py
+++ augment/rl/augmentation_functions/panda.py
@@ -124,11 +124,9 @@ class HERTranslateGoalProximal(HERMixed):
     def __init__(self, env, strategy='future', p=0.5, **kwargs):
         super().__init__(env=env, aug_function=TranslateGoalProximal, strategy=strategy, p=p, **kwargs)
 
-class HERReflect(HERMixed):
-    def __init__(self, env, strategy='future', p=0.5, **kwargs):
-        super().__init__(env=env, aug_function=Reflect, strategy=strategy, p=p, **kwargs)
-
-
+# class HERReflect(HERMixed):
+#     def __init__(self, env, strategy='future', p=0.5, **kwargs):
+#         super().__init__(env=env, aug_function=Reflect, strategy=strategy, p=p, **kwargs)
 
 class RobotAugmentationFunction(HERAugmentationFunction):
     def __init__(self, env, **kwargs):
@@ -166,23 +164,37 @@ class RobotAugmentationFunction(HERAugmentationFunction):
 
         return obs, next_obs, action, reward, done, infos
 
-class ReachReflect(AugmentationFunction):
+class ReachReflect(RobotAugmentationFunction):
 
     def __init__(self, env,  **kwargs):
         super().__init__(env=env, **kwargs)
         self.env = env
         self.delta = 0.05
 
-
     def _set_done_and_info(self, done, infos, at_goal):
         done |= at_goal
         infos[done & ~at_goal] = [{'TimeLimit.truncated': True}]
         infos[done & at_goal] = [{'TimeLimit.truncated': False}]
 
+    def _reflect_robot_obs(self, obs):
+        # y reflection
+        obs[:, 1] *= -1
+        obs[:, 4] *= -1
+
+    def _reflect_object_obs(self, obs):
+        # y reflection
+        obs[:, 7] *= -1
+        obs[:, 13] *= -1
+        obs[:, 16] *= -1
+
+    def _reflect_goal_obs(self, obs):
+        # y reflection
+        obs[:, self.env.goal_idx[1]] *= -1
+
     def _reflect_obs(self, obs):
-        obs[:, self.env.achieved_idx[0]] *= -1
-        obs[:, self.env.goal_idx[0]] *= -1
-        obs[:, 3] *= -1 # xvel
+        self._reflect_robot_obs(obs)
+        self._reflect_object_obs(obs)
+        self._reflect_goal_obs(obs)
 
     def _augment(self,
                  obs: np.ndarray,
@@ -197,40 +209,10 @@ class ReachReflect(AugmentationFunction):
         self._reflect_obs(obs)
         self._reflect_obs(next_obs)
 
-        action[:, 0] *= -1
+        action[:, 1] *= -1
 
         return obs, next_obs, action, reward, done, infos
 
-class Translate(AugmentationFunction):
-
-    def __init__(self, env,  **kwargs):
-        super().__init__(env=env, **kwargs)
-        self.env = env
-        self.delta = 0.05
-
-
-    def _set_done_and_info(self, done, infos, at_goal):
-        done |= at_goal
-        infos[done & ~at_goal] = [{'TimeLimit.truncated': True}]
-        infos[done & at_goal] = [{'TimeLimit.truncated': False}]
-
-    def _augment(self,
-                 obs: np.ndarray,
-                 next_obs: np.ndarray,
-                 action: np.ndarray,
-                 reward: np.ndarray,
-                 done: np.ndarray,
-                 infos: List[Dict[str, Any]],
-                 p=None,
-                 ):
-
-        obs[:, self.env.achieved_idx[0]] *= -1
-        next_obs[:, self.env.achieved_idx[0]] *= -1
-        next_obs[:, self.env.goal_idx[0]] *= -1
-
-        action[:, 0] *= -1
-
-        return obs, next_obs, action, reward, done, infos
 
 
 # Reach, Push, Slide, PickAndPlace only
@@ -242,3 +224,8 @@ PANDA_AUG_FUNCTIONS = {
     'translate_goal_proximal': TranslateGoalProximal,
     'reflect': ReachReflect, # Does not apply to flip no stack
 }
+
+PANDA_REACH_AUG_FUNCTIONS = {
+    'reflect': ReachReflect,
+}
+PANDA_REACH_AUG_FUNCTIONS.update(PANDA_AUG_FUNCTIONS)
