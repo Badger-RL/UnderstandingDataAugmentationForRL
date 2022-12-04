e17b6b8393f9d583a5308cca1ac9597e6735b7ed
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2022-12-04 13:47:16 -0600

    common

--- augment/rl/augmentation_functions/fetch/common.py
+++ augment/rl/augmentation_functions/fetch/common.py
@@ -1,89 +1,104 @@
 from typing import Dict, List, Any
 import numpy as np
 
-from augment.rl.augmentation_functions.augmentation_function import AugmentationFunction
+from augment.rl.augmentation_functions.augmentation_function import AugmentationFunction, HERAugmentationFunction
 
-class FetchReachAugmentationFunction(AugmentationFunction):
-
-    def __init__(self, env,  **kwargs):
+class GoalAugmentationFunction(HERAugmentationFunction):
+    def __init__(self, env, **kwargs):
         super().__init__(env=env, **kwargs)
-        self.env = env
-        self.lo = env.initial_gripper_xpos[:3] - env.target_range
-        self.hi = env.initial_gripper_xpos[:3] + env.target_range
         self.delta = 0.05
-        self.desired_mask = env.desired_mask
-        self.achieved_mask = env.achieved_mask
+
+        self.goal_length = self.env.goal_idx.shape[-1]
+
+    def _sample_goals(self, next_obs, n):
+        raise NotImplementedError()
 
     def _set_done_and_info(self, done, infos, at_goal):
         done |= at_goal
         infos[done & ~at_goal] = [{'TimeLimit.truncated': True}]
         infos[done & at_goal] = [{'TimeLimit.truncated': False}]
 
-class FetchHER(FetchReachAugmentationFunction):
+    def _augment(self,
+                 obs: np.ndarray,
+                 next_obs: np.ndarray,
+                 action: np.ndarray,
+                 reward: np.ndarray,
+                 done: np.ndarray,
+                 infos: List[Dict[str, Any]],
+                 p=None,
+                 ):
+
+        n = obs.shape[0]
+        new_goal = self._sample_goals(next_obs, n)
+        obs[:, self.env.goal_idx] = new_goal
+        next_obs[:, self.env.goal_idx] = new_goal
+
+        achieved_goal = next_obs[:, self.env.achieved_idx]
+        at_goal = self.env.task.is_success(achieved_goal, new_goal).astype(bool)
+        reward = self.env.task.compute_reward(achieved_goal, new_goal, infos)
+        self._set_done_and_info(done, infos, at_goal)
+
+        return obs, next_obs, action, reward, done, infos
+
+class TranslateGoal(GoalAugmentationFunction):
 
     def __init__(self, env,  **kwargs):
         super().__init__(env=env, **kwargs)
 
-    def _augment(self,
-                 obs: np.ndarray,
-                 next_obs: np.ndarray,
-                 action: np.ndarray,
-                 reward: np.ndarray,
-                 done: np.ndarray,
-                 infos: List[Dict[str, Any]],
-                 p=None,
-                 ):
+    def _sample_goals(self, next_obs, n):
+        return self.env.task._sample_n_goals(n)
 
-        obs[:, self.desired_mask] = obs[-1, self.achieved_mask]
-        next_obs[:, self.desired_mask] = next_obs[-1, self.achieved_mask]
-        at_goal = self.env.is_success(next_obs[:, self.achieved_mask], next_obs[:, self.desired_mask]).astype(bool)
-        reward = self.env.compute_reward(next_obs[:, self.achieved_mask], next_obs[:, self.desired_mask], infos)
+class TranslateGoalProximal(GoalAugmentationFunction):
 
-        self._set_done_and_info(done, infos, at_goal)
-
-        end = np.argmax(done)+1
-        obs = obs[:end]
-        next_obs = next_obs[:end]
-        action = action[:end]
-        reward = reward[:end]
-        done = done[:end]
-        infos = infos[:end]
-
-
-        return obs, next_obs, action, reward, done, infos
-
-class FetchTranslateGoal(FetchReachAugmentationFunction):
-
-    def __init__(self, env, use_z=False, **kwargs):
+    def __init__(self, env, p=0.5,  **kwargs):
         super().__init__(env=env, **kwargs)
+        self.p = p
 
-    def _augment(self,
-                 obs: np.ndarray,
-                 next_obs: np.ndarray,
-                 action: np.ndarray,
-                 reward: np.ndarray,
-                 done: np.ndarray,
-                 infos: List[Dict[str, Any]],
-                 p=None,
-                 ):
+    def _sample_goals(self, next_obs, n):
+        if np.random.random() < self.p:
+            r = np.random.uniform(0, self.delta)
+            theta = np.random.uniform(-np.pi, np.pi)
+            phi = np.random.uniform(-np.pi/2, np.pi/2)
+            dx = r*np.sin(phi)*np.cos(theta)
+            dy = r*np.sin(phi)*np.sin(theta)
+            dz = r*np.cos(phi)
+            if self.env.task.goal_range_high[-1] == 0:
+                dz = 0
+            new_goal = next_obs[:, self.env.goal_idx] + np.array([dx, dy, dz])
+        else:
+            new_goal = self.env.task._sample_n_goals(n)
 
-        new_goal = np.random.uniform(self.lo, self.hi)
+        return new_goal
 
-        obs[:, self.desired_mask] = new_goal
-        next_obs[:, self.desired_mask] = new_goal
-
-        at_goal = self.env.is_success(next_obs[:, self.achieved_mask], next_obs[:, self.desired_mask]).astype(bool)
-        reward = self.env.compute_reward(next_obs[:, self.achieved_mask], next_obs[:, self.desired_mask], infos)
-
-        self._set_done_and_info(done, infos, at_goal)
-
-        return obs, next_obs, action, reward, done, infos
-
-
-class FetchTranslateGoalProximal(FetchReachAugmentationFunction):
-
-    def __init__(self, env, p=0.5, **kwargs):
+class HER(GoalAugmentationFunction):
+    def __init__(self, env, strategy='future', **kwargs):
         super().__init__(env=env, **kwargs)
+        self.delta = 0.05
+        self.strategy = strategy
+        if self.strategy == 'future':
+            self.goal_sampler = self._sample_future
+        else:
+            self.goal_sampler = self._sample_last
+
+    def _sample_future(self, next_obs, n):
+        low = np.arange(n)
+        indices = np.random.randint(low=low, high=n)
+        final_pos = next_obs[indices].copy()
+        final_pos = final_pos[:, self.env.achieved_idx]
+        return final_pos
+
+    def _sample_last(self, next_obs, n):
+        final_pos = next_obs[:, self.env.achieved_idx].copy()
+        return final_pos
+
+    def _sample_goals(self, next_obs, n):
+        return self.goal_sampler(next_obs, n)
+
+class HERMixed(GoalAugmentationFunction):
+    def __init__(self, env, aug_function, strategy='future', p=0.5, **kwargs):
+        super().__init__(env, **kwargs)
+        self.HER = HER(env, strategy, **kwargs)
+        self.aug_function = aug_function(env, **kwargs)
         self.p = p
 
     def _augment(self,
@@ -97,22 +112,23 @@ class FetchTranslateGoalProximal(FetchReachAugmentationFunction):
                  ):
 
         if np.random.random() < self.p:
-            r = np.random.uniform(0, self.delta)
-            theta = np.random.uniform(-np.pi, np.pi)
-            phi = np.random.uniform(-np.pi/2, np.pi/2)
-            dx = r*np.sin(phi)*np.cos(theta)
-            dy = r*np.sin(phi)*np.sin(theta)
-            dz = 0 # r*np.cos(phi)
-            new_goal = obs[:, self.desired_mask] + np.array([dx, dy, dz])
+            return self.HER._augment(obs, next_obs, action, reward, done, infos, p)
         else:
-            new_goal = np.random.uniform(self.lo, self.hi)
+            return self.aug_function._augment(obs, next_obs, action, reward, done, infos, p)
 
-        obs[:, self.desired_mask] = new_goal
-        next_obs[:, self.desired_mask] = new_goal
+class HERTranslateGoal(HERMixed):
+    def __init__(self, env, strategy='future', p=0.5, **kwargs):
+        super().__init__(env=env, aug_function=TranslateGoal, strategy=strategy, p=p, **kwargs)
 
-        at_goal = self.env.is_success(next_obs[:, self.achieved_mask], next_obs[:, self.desired_mask]).astype(bool)
-        reward = self.env.compute_reward(next_obs[:, self.achieved_mask], next_obs[:, self.desired_mask], infos)
+class HERTranslateGoalProximal(HERMixed):
+    def __init__(self, env, strategy='future', p=0.5, **kwargs):
+        super().__init__(env=env, aug_function=TranslateGoalProximal, strategy=strategy, p=p, **kwargs)
 
-        self._set_done_and_info(done, infos, at_goal)
-
-        return obs, next_obs, action, reward, done, infos
+# Reach, Push, Slide, PickAndPlace only
+PANDA_AUG_FUNCTIONS = {
+    'her': HER,
+    'her_translate_goal': HERTranslateGoal,
+    'her_translate_goal_proximal': HERTranslateGoalProximal,
+    'translate_goal': TranslateGoal,
+    'translate_goal_proximal': TranslateGoalProximal,
+}
