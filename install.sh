fcc038463bd337e7737a2c6ae588860bb7d8a5c5
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2022-12-09 19:36:47 -0600

    gpu

--- install.sh
+++ install.sh
@@ -4,7 +4,12 @@
 ENVNAME=rl-augment
 ENVDIR=${ENVNAME}_env
 cp /staging/ncorrado/${ENVNAME}.tar.gz .
+
 mkdir $ENVDIR
+if [${1}] then
+  ENVNAME=rl-augment_gpu
+fi
+
 tar -xzf ${ENVNAME}.tar.gz -C $ENVDIR
 rm ${ENVNAME}.tar.gz # remove env tarball
 source $ENVDIR/bin/activate
