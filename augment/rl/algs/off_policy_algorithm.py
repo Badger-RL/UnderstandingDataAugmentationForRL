1d4fba555c5d662c26312ea8e7f17317dfc77855
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-01-13 17:30:19 -0600

    use most recent for coda

--- augment/rl/algs/off_policy_algorithm.py
+++ augment/rl/algs/off_policy_algorithm.py
@@ -484,8 +484,7 @@ class OffPolicyAlgorithmAugment(OffPolicyAlgorithm):
         num_coda_samples_made = 0
         coda_n = self.coda_n_floor + int(np.random.random() < self.coda_prob)
         while num_coda_samples_made < coda_n:
-            observation0, action0, next_observation0, reward0, done0, timeout0 = self.replay_buffer.sample_array(
-                batch_size=1)
+            observation0, action0, next_observation0, reward0, done0, timeout0 = self.replay_buffer.sample_array_most_recent()
             observation1, action1, next_observation1, reward1, done1, timeout1 = self.replay_buffer.sample_array(
                 batch_size=1)
 
