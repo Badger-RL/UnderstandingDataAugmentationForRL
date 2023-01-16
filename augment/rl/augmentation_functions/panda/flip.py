ef4c9cfb71691a9ffb3b7dcb80d9ca61c74977e3
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-01-15 18:52:42 -0600

    bug fix for translate object

--- augment/rl/augmentation_functions/panda/flip.py
+++ augment/rl/augmentation_functions/panda/flip.py
@@ -3,7 +3,8 @@ from typing import Any, List, Dict
 
 import numpy as np
 
-from augment.rl.augmentation_functions.panda.common import GoalAugmentationFunction, PANDA_AUG_FUNCTIONS, HER, HERMixed
+from augment.rl.augmentation_functions.panda.common import GoalAugmentationFunction, PANDA_AUG_FUNCTIONS, HER, HERMixed, \
+    ObjectAugmentationFunction
 
 
 class TranslateGoalProximal(GoalAugmentationFunction):
@@ -99,10 +100,33 @@ class HERTranslateGoalProximal09(GoalAugmentationFunction):
         else:
             return self.aug_function._augment(obs, next_obs, action, reward, done, infos)
 
+
+class TranslateObject(ObjectAugmentationFunction):
+
+    def __init__(self, env,  **kwargs):
+        super().__init__(env=env, **kwargs)
+        self.obj_pos_mask = np.zeros_like(self.env.obj_idx, dtype=bool)
+        obj_pos_idx = np.argmax(self.env.obj_idx)
+        self.obj_pos_mask[obj_pos_idx:obj_pos_idx+3] = True
+
+        self.obj_vel_mask = np.zeros_like(self.env.obj_idx, dtype=bool)
+        self.obj_vel_mask[obj_pos_idx+3:-3] = True
+
+    def _sample_object(self, next_obs):
+        ep_length = next_obs.shape[0]
+        new_obj, new_rot = self.env.task._sample_n_objects(ep_length) # new_rot = np.zeros(3)
+        return new_obj
+
+class HERTranslateObject(HERMixed):
+    def __init__(self, env, strategy='future', q=0.5, **kwargs):
+        super().__init__(env=env, aug_function=TranslateObject, strategy=strategy, q=q, **kwargs)
+
 PANDA_FLIP_AUG_FUNCTIONS = copy.deepcopy(PANDA_AUG_FUNCTIONS)
 PANDA_FLIP_AUG_FUNCTIONS.update(
     {
         'translate_goal_proximal': TranslateGoalProximal,
         'translate_goal_proximal_0': TranslateGoalProximal0,
         'her_translate_goal_proximal_0': TranslateGoalProximal,
+        'translate_object': TranslateObject,
+        'her_translate_object': HERTranslateObject,
     })
