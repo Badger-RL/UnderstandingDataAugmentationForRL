07fb36cc9daa4dde232991c5e058016c63c9d5b4
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air.local>
2023-02-01 15:17:35 -0600

    Fetch

---
+++ augment/my-gym/my_gym/envs/fetch/__init__.py
@@ -0,0 +1,4 @@
+from my_gym.envs.fetch.reach import MujocoFetchReachEnv
+from my_gym.envs.fetch.push import MujocoFetchPushEnv
+from my_gym.envs.fetch.slide import MujocoFetchSlideEnv
+from my_gym.envs.fetch.pick_and_place import MujocoFetchPickAndPlaceEnv
